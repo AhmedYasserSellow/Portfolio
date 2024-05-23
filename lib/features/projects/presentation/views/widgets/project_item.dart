d505f2d4a542d79f64430589c7db18eb824db346
Ahmed Yasser <ahmedyasser3554@gmail.com>
2024-05-23 06:01:39 +0300

    fix project issue

--- lib/features/projects/presentation/views/widgets/project_item.dart
+++ lib/features/projects/presentation/views/widgets/project_item.dart
@@ -4,6 +4,7 @@ import 'package:portfolio/core/utils/app_text_styles.dart';
 import 'package:portfolio/features/drawer/data/models/social_media_model.dart';
 import 'package:portfolio/features/drawer/presentation/views/widgets/social_media_button.dart';
 import 'package:portfolio/features/projects/data/models/project_model.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ProjectItem extends StatelessWidget {
   const ProjectItem({
@@ -13,52 +14,57 @@ class ProjectItem extends StatelessWidget {
   final ProjectModel projectModel;
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              projectModel.name,
-              style: AppTextStyles.ktsHeader,
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-            ),
-            const Expanded(
-              child: SizedBox(
-                height: 8,
+    return GestureDetector(
+      onTap: () {
+        launchUrl(Uri.parse(projectModel.link));
+      },
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                projectModel.name,
+                style: AppTextStyles.ktsHeader,
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
               ),
-            ),
-            Text(
-              projectModel.description,
-              style: AppTextStyles.ktsNormal,
-              maxLines: 3,
-              overflow: TextOverflow.ellipsis,
-            ),
-            const Expanded(
-              child: SizedBox(
-                height: 8,
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Check on github',
-                  style: AppTextStyles.ktsNormal.copyWith(
-                    color: Colors.white,
-                  ),
+              const Expanded(
+                child: SizedBox(
+                  height: 8,
                 ),
-                const SocialMediaButton(
-                  socialMediaModel: SocialMediaModel(
-                    imagePath: Assets.imagesGithub,
-                    linkURL: '',
+              ),
+              Text(
+                projectModel.description,
+                style: AppTextStyles.ktsNormal,
+                maxLines: 3,
+                overflow: TextOverflow.ellipsis,
+              ),
+              const Expanded(
+                child: SizedBox(
+                  height: 8,
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Check on github',
+                    style: AppTextStyles.ktsNormal.copyWith(
+                      color: Colors.white,
+                    ),
                   ),
-                )
-              ],
-            ),
-          ],
+                  const SocialMediaButton(
+                    socialMediaModel: SocialMediaModel(
+                      imagePath: Assets.imagesGithub,
+                      linkURL: '',
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
